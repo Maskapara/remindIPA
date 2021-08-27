229e453e1f0503959bf44cc3fa66c632c5b6fa28
masu <bloodteam.a.ranger@gmail.com>
2021-08-28 01:15:27 +0900

    add function

--- sh/checkNextIPA.sh
+++ sh/checkNextIPA.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 thisYear=`date +%Y`;
-thisJapanYear=`expr $thisYear \- 2019 \+ 1`;
-
 thisMonth=`date +%-m`;
-nextSeason="haru";
 
-if [ 4 -le ${thisMonth} ] && [ ${thisMonth} -le 9 ] ; then
-    nextSeason="aki"
-elif [ 9 -lt ${thisMonth} ] ; then
-    thisJapanYear=thisJapanYear+1
-fi
-    
-thisJapanYear="0${thisJapanYear}";
-thisJapanYear="${thisJapanYear:-2}";
+callRequest() {
 
-curl -l https://www.jitec.ipa.go.jp/1_02annai/r${thisJapanYear}${nextSeason}_exam.html -o /dev/null -w '%{http_code}\n' -s;
+    thisGengouYear=`expr $1 \- 2019 \+ 1`;
+    nextSeason="haru";
+
+    if [ 4 -le ${thisMonth} ] && [ ${thisMonth} -le 9 ] ; then
+        nextSeason="aki"
+    elif [ 9 -lt ${thisMonth} ] ; then
+        thisGengouYear=thisGengouYear+1
+    fi
+
+    thisGengouYear="0${thisGengouYear}";
+    thisGengouYear="${thisGengouYear:-2}";
+
+    curl -l https://www.jitec.ipa.go.jp/1_02annai/r${thisGengouYear}${nextSeason}_exam.html -o /dev/null -w '%{http_code}\n' -s;
+
+}
+
+callRequest ${thisYear} ${thisMonth}
